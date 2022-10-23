5dcc40c91be6a4dcfd2dcdd961e53ac4756e5863
KatsFerDays <N/A>
2022-10-22 23:08:21 -0400

    fix target name

--- xmake.lua
+++ xmake.lua
@@ -3,8 +3,9 @@ set_version("1.0.0")
 
 add_requires("cpr", "nlohmann_json")
 
-target("RbxUserInfo")
+target("libRbxUserInfo")
     set_kind("$(kind)")
+    set_prefixname("")
     add_files("src/libRbxUserInfo.cpp")
     add_includedirs("include")
     add_packages("cpr", "nlohmann_json")
