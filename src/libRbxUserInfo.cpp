bb93590d8f73fae4311573451d30d80f6cc1a58b
KatsFerDays <N/A>
2022-10-23 17:32:19 -0400

    my mental state is rapidly deteriorating

--- src/libRbxUserInfo.cpp
+++ src/libRbxUserInfo.cpp
@@ -1,27 +1,33 @@
 #include <cpr/cpr.h>
 #include <nlohmann/json.hpp>
 #include "libRbxUserInfo.hpp"
+#include <iostream>
 
 namespace RbxUserInfo {
     using json = nlohmann::json;
 
     User MakeUser(json userInfoParse, json onlineStatParse) {
         User out;
-        out.username = userInfoParse.at("name").get<std::string>();
-        out.displayName = userInfoParse.at("displayName").get<std::string>();
-        out.creationDate = userInfoParse.at("created").get<std::string>();
-        out.description = userInfoParse.at("description").get<std::string>();
-        out.lastLocation = onlineStatParse.at("LastLocation").get<std::string>();
-        out.lastOnline = onlineStatParse.at("LastOnline").get<std::string>();
-        out.userID = userInfoParse.at("id").get<int>();
-        out.locationType = onlineStatParse.at("LocationType").get<int>();
-        out.gameID = onlineStatParse.at("GameId").get<int>();
-        out.placeID = onlineStatParse.at("PlaceId").get<int>();
-        out.presenceType = onlineStatParse.at("PresenceType").get<int>();
-        out.universeID = onlineStatParse.at("UniverseId").get<int>();
-        out.verified = userInfoParse.at("hasVerifiedBadge").get<bool>();
-        out.banned = userInfoParse.at("isBanned").get<bool>();
-        out.isOnline = onlineStatParse.at("IsOnline").get<bool>();
+        try {
+            out.username = userInfoParse.at("name").get<std::string>();
+            out.displayName = userInfoParse.at("displayName").get<std::string>();
+            out.creationDate = userInfoParse.at("created").get<std::string>();
+            out.description = userInfoParse.at("description").get<std::string>();
+            out.lastLocation = onlineStatParse.at("LastLocation").get<std::string>();
+            out.lastOnline = onlineStatParse.at("LastOnline").get<std::string>();
+            out.userID = userInfoParse.at("id").get<int>();
+            out.locationType = onlineStatParse.at("LocationType").get<int>();
+            out.gameID = onlineStatParse.at("GameId").get<int>();
+            out.placeID = onlineStatParse.at("PlaceId").get<int>();
+            out.presenceType = onlineStatParse.at("PresenceType").get<int>();
+            out.universeID = onlineStatParse.at("UniverseId").get<int>();
+            out.verified = userInfoParse.at("hasVerifiedBadge").get<bool>();
+            out.banned = userInfoParse.at("isBanned").get<bool>();
+            out.isOnline = onlineStatParse.at("IsOnline").get<bool>();
+        }
+        catch(std::exception& e) {
+            std::cerr << e.what();
+            system("pause");
         return out;
     }
 
